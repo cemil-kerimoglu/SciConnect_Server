1391043dc815ee6d716364a5e52049449f6a4e0b
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2022-09-22 14:02:36 +0200

    introduced pagination

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,9 +2,15 @@ import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
+    const { page } = req.query;
+
     try {
-        const postMessages = await PostMessage.find();
-        res.status(200).send(postMessages);
+        const limit = 6;
+        const startIndex = (Number(page) - 1) * limit;
+        const total = await PostMessage.countDocuments({});
+
+        const posts = await PostMessage.find().sort({ _id: -1 }).limit(limit).skip(startIndex);
+        res.status(200).send({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / limit) });
     } catch (error) {
         res.status(404).send({ message: error.message });
     }
